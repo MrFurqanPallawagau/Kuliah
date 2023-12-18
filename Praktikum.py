abd699830e32c2bb9096df37fffe996fce4f29d4
MrFurqanPallawagau <153912134+MrFurqanPallawagau@users.noreply.github.com>
2023-12-18 09:34:09 +0800

    Add files via upload

---
+++ Praktikum.py
@@ -0,0 +1,154 @@
+print("praktikum-d1")
+print()
+print("Nama   :A.Alfurqan pallawagau")
+print("NIM    :231031105")
+print("prodi  :Sistem Informasi")
+
+angkatan = 2023
+print('angkatan adalah ',angkatan)
+
+print()
+a = 20
+print('data adalah = ',a )
+print('tipenya adalah = ',type(a))
+
+print()
+b = 20.0
+print('data adalah =',b )
+print('tipenya adalah =',type(b) )
+
+print()
+kampus = 'Insitut Teknologi BJ Habibie'
+print('data adalah =' ,kampus)
+print('tipenya adalah =', type(kampus))
+
+print()
+log = True
+print('data adalah =' ,log)
+print('tipenya adalah =', type(log))
+
+print()
+c = complex(5,8)
+print('data adalah =' ,c)
+print('tipenya adalah =', type(c))
+
+print()
+p = 20
+q = 5
+hasil = p + q
+print('hasil',p,'+',q,'adalah',hasil)
+
+print()
+p = 20
+q = 5
+hasil = p - q
+print('hasil',p,'-',q,'adalah',hasil)
+
+print()
+p = 20
+q = 5
+hasil = p * q
+print('hasil',p,'x',q,'adalah',hasil)
+
+print()
+p = 20
+q = 3
+hasil = p / q
+print('hasil',p,'/',q,'adalah',hasil)
+
+print()
+p = 37
+q = 31
+hasil = p % q
+print('hasil',p,'%',q,'adalah',hasil)
+
+print()
+p = 37
+q = 31
+hasil = p // q
+print('hasil',p,'//',q,'adalah',hasil)
+
+print ()
+a = 25
+print('Nilai a adalah',a)
+a -= 2   #ini adalah a = a + 1
+print('nilai a-=2 menjadi',a)
+
+print()
+
+a = 20
+print('Nilai a adalah',a)
+a *= 2
+print('nilai a*=2 menjadi',a)
+
+print()
+a = 20
+print('Nilai a adalah',a)
+a /= 5
+print('nilai a/=2 menjadi',a)
+
+print()
+a = 20
+print("nilai a adalah",a)
+a %= 7
+print('nilai a%= adlah ',a)
+
+print()
+a = 20
+print("nilai a adalah",a)
+a //= 7
+print('nilai a//= adalah ',a)
+
+print()      #ini operasi OR
+log2 = True
+print('nilai log adalah', log2)
+log2 |= False  #Ini perintah log=false log
+print('Nilai log = False Menjadi',log2)
+
+print()      #ini operasi AND
+log2 = True
+print('nilai log adalah', log2)
+log2 &= True  #Ini perintah log= log & false
+print('Nilai log = False Menjadi',log2)
+
+print()      #ini operasi XOR
+log2 = True
+print('nilai log adalah', log2)
+log2 ^= True    #Ini perintah log= log & false
+print('Nilai log = True Menjadi',log2)
+
+print() #ini operasi shifting
+bi = 0b0100
+print('Nilai bi =',format(bi,'04b'))
+bi <<= 1      #menggeser digitt ke kiri 1 kali
+print('nilai bi <<=1 menjadi',format(bi,'04b'))
+
+print()
+a = 20
+hasil = a>15
+print('hasil',a,'>15 adalah',hasil)
+
+x = 5
+y = 3
+print(x == y)  # False
+
+x = 5
+y = 3
+print(x != y)  # True
+
+x = 5
+y = 3
+print(x > y)  # True
+
+x = 5
+y = 3
+print(x < y)  # False
+
+x = 5
+y = 5
+print(x >= y)  # True
+
+x = 5
+y = 3
+print(x <= y)  # False
+
